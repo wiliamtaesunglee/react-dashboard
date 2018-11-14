ddccc97a27c7bcc45373f5c876113a58043175d8
wiliamtaesunglee <wiliam.tae@gmail.com>
2018-11-14 16:43:52 -0200

    folder build adicted

---
+++ build/precache-manifest.ef8af661539f19f0720006444a370ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b349aa443e73932b99fb",
+    "url": "/static/js/main.b349aa44.chunk.js"
+  },
+  {
+    "revision": "80193ff7cd106ffdb397",
+    "url": "/static/js/1.80193ff7.chunk.js"
+  },
+  {
+    "revision": "b349aa443e73932b99fb",
+    "url": "/static/css/main.c337b746.chunk.css"
+  },
+  {
+    "revision": "80193ff7cd106ffdb397",
+    "url": "/static/css/1.782eb270.chunk.css"
+  },
+  {
+    "revision": "82d75c01ce42fbc90f0017a4a31ea98d",
+    "url": "/index.html"
+  }
+];
